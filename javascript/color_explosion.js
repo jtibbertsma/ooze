d53c13432c5f47b6d3b49bd185249834c90587dd
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-08-01 20:34:54 -0700

    Finished primordial ooze

---
+++ javascript/color_explosion.js
@@ -0,0 +1,131 @@
+$(function () {
+  $.ExplosionTile = function (tile) {
+    this.$tile = $(tile);
+    this.pos = this.$tile.data("pos");
+
+    this.$tile.data("explosion", this);
+
+    this.$tile.on("mouseover", this.madTransitions.bind(this));
+  };
+
+  $.fn.colorExplosion = function () {
+    return this.find(".explosion-tile").each(function () {
+      new $.ExplosionTile(this);
+    })
+  };
+
+  var NEIGHBOR_DELTAS = [
+              [-1, 0],
+
+    [ 0, -1],          [ 0, 1],
+
+              [ 1, 0]
+  ];
+
+  // var NEIGHBOR_DELTAS = [
+  //   [-1, -1], [-1, 0], [-1, 1],
+
+  //   [ 0, -1],          [ 0, 1],
+
+  //   [ 1, -1], [-1, 0], [ 1, 1]
+  // ];
+
+  // var NEIGHBOR_DELTAS = [
+  //   [-1, -1],          [-1, 1],
+
+
+
+  //   [ 1,  1],          [ 1, 1],
+  // ];
+
+  $.ExplosionTile.prototype.neighbors = function () {
+    var thisPos = this.pos;
+
+    return $(NEIGHBOR_DELTAS
+      .map(function (delta) {
+        return [thisPos[0] + delta[0], thisPos[1] + delta[1]];
+      })
+      .filter(function (pos) {
+        var r = pos[0], c = pos[1];
+
+        if (r < 0 || r > window.EXPLOSION_ROWS ||
+            c < 0 || c > window.EXPLOSION_COLS) {
+          return false;
+        }
+
+        return true;
+      })
+      .map(function (pos) {
+        var r = pos[0], c = pos[1];
+
+        return $(".color-explosion")
+          .find(".explosion-row")
+          .eq(r)
+          .find(".explosion-tile")
+          .eq(c)
+          .data("explosion");
+      })
+    );
+  };
+
+  $.ExplosionTile.prototype.madTransitions = function () {
+    // comment out lines with // !!!
+    // to see something cool
+    // but it eats up a lot of cpu
+
+    if (this.transitioning) { return; } // !!!
+    var that = this;                    // !!!
+    var $tile = this.$tile;
+    $tile.removeClass("explosion-red explosion-blue explosion-green " +
+                      "explosion-yellow explosion-orange explosion-purple"
+    );
+    
+    that.transitioning = true;          // !!!
+    
+    $tile.addClass("explosion-yellow");
+
+    // TODO: refactor
+    setTimeout(function () {
+      $tile.removeClass("explosion-yellow");
+      $tile.addClass("explosion-orange");
+
+      setTimeout(function () {
+        $tile.removeClass("explosion-orange")
+        $tile.addClass("explosion-red");
+
+        setTimeout(function () {
+          $tile.removeClass("explosion-red")
+          $tile.addClass("explosion-purple");
+
+          setTimeout(function () {
+            $tile.removeClass("explosion-purple");
+            $tile.addClass("explosion-blue");
+
+            setTimeout(function () {
+              $tile.removeClass("explosion-blue");
+              $tile.addClass("explosion-green");
+
+              setTimeout(function () {
+                $tile.removeClass("explosion-green");
+                $tile.addClass("explosion-yellow");
+
+                setTimeout(function () {
+                  $tile.removeClass("explosion-yellow");
+                  that.transitioning = false; // !!!
+                }, 400);
+              }, 400);
+            }, 400);
+          }, 400);
+        }, 400);
+      }, 400);
+    }, 400);
+
+    setTimeout(function () {
+      this.neighbors().each(function(idx, $neighbor) {
+        $neighbor.madTransitions();
+      });
+    }.bind(this), 20);
+  };
+
+  $(".color-explosion").colorExplosion();
+});
---
+++ javascript/insert_tiles.js
@@ -0,0 +1,80 @@
+$(function () {
+  var working = false;
+  
+  var doInsert = function () {
+    if (working) { return; }
+    working = true;
+
+    var $mainDiv = $(".color-explosion"),
+
+        tilesPerRow = Math.floor($(document).width() / 10),
+        leftoverWidth = $(document).width() - (tilesPerRow * 10),
+
+        numRows = Math.floor($(document).height() / 10),
+        leftoverHeight = $(document).height() - (tilesPerRow * 10),
+        $row, i, j;
+
+
+    $mainDiv.empty();
+
+    window.EXPLOSION_ROWS = numRows;
+    window.EXPLOSION_COLS = tilesPerRow;
+
+    // TODO: refactor?
+    for (i = 0; i < numRows; ++i) {
+      $row = $("<div>").addClass("explosion-row");
+
+      for (j = 0; j < tilesPerRow; ++j) {
+        $row.append(
+          $("<div>")
+            .addClass("explosion-tile")
+            .data("pos", [i, j])
+        );
+      }
+
+      if (leftoverWidth > 0) {
+        $row.append(
+          $("<div>")
+            .addClass("explosion-tile")
+            .css("width", '' + leftoverWidth + 'px')
+            .data("pos", [i, tilesPerRow])
+        )
+      }
+
+      $mainDiv.append($row);
+    }
+
+    if (leftoverHeight > 0) {
+      $row = $("<div>").addClass("explosion-row");
+      for (i = 0; i < tilesPerRow; ++i) {
+        $row.append(
+          $("<div>")
+            .addClass("explosion-row")
+            .css("height", '' + leftoverHeight + 'px')
+            .data("pos", [numRows, i])
+        )
+        window.EXPLOSION_ROWS += 1;
+      }
+      if (leftoverWidth > 0) {
+        $row.append(
+          $("<div>")
+            .addClass("explosion-tile")
+            .css("height", '' + leftoverHeight + 'px')
+            .css("width", '' + leftoverWidth + 'px')
+            .data("pos", [numRows, tilesPerRow])
+        )
+        // XXX: Won't be set properly if vertical pixels % 10 is 0
+        window.EXPLOSION_COLS += 1;
+      }
+      $mainDiv.append($row);
+    }
+
+    working = false;
+  };
+
+  doInsert();
+
+  // uncomment and all your resources are belong to us if window is resized
+  // I don't know why this doesn't work properly
+  //$(window).on('resize', doInsert);
+})
